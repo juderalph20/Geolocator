568af62889dd04440ce508b839189c5752011daf
juderalph20 <edujhplar@gmail.com>
2025-05-22 06:02:16 +0800

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,7 +7,26 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  latitude: number | null = null;
+  longitude: number | null = null;
+  error: string | null = null;
 
   constructor() {}
 
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.latitude = position.coords.latitude;
+          this.longitude = position.coords.longitude;
+          this.error = null;
+        },
+        (err) => {
+          this.error = 'Geolocation error: ' + err.message;
+        }
+      );
+    } else {
+      this.error = 'Geolocation is not supported by this browser.';
+    }
+  }
 }
